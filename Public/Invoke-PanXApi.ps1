7f2654f02b8c755c972d7319291cb1cacd5edcd1
John Urbanek <jurbanek@gmail.com>
2023-04-04 20:30:51 -0500

    Updated Invoke-PanXApi to support import, New New-MultipartFormData

--- Public/Invoke-PanXApi.ps1
+++ Public/Invoke-PanXApi.ps1
@@ -1,19 +1,30 @@
 function Invoke-PanXApi {
    <#
    .SYNOPSIS
+   Abstracts PAN-OS XML API core modes (actions)
    .DESCRIPTION
+   -Keygen is for generating API key
+   -Version is an easy way to verify API access is functioning
+
+   -Config -Show retrieves ACTIVE configuration.
+   -Config -Show only works when provided XPath specifies single node.
+   -Config -Show can use relative XPath.
+
+   -Config -Get retrieves CANDIDATE, uncommitted configuration.
+   -Config -Get works with single and multiple nodes.
+   -Config -Get requires absolute XPath.
+
+   -Config -Set adds, updates, or merges configuration nodes. -Config -Set actions are non-destructive and are only additive.
+   -Config -Edit replaces configuration nodes. -Config -Edit actions can be destructive.
+
+   -Import -Category "certificate" is for uploading certificates WITHOUT private key (processes just the certificate)
+   -Import -Category "keypair" is for uploading certificates WITH private key (processes both certificate and private key)
+      -File is the path or FileInfo object to the supported (PKCS12, Base64 encoded PEM) certificate on local disk 
+      -CertName parameter is what is used for PAN-OS certificate name, the certificate filename on local disk is ignored
+         -Periods (.) are ignored/removed by PAN-OS. Avoid them in CertName
+      -CertPassphrase is used when importing the private key
+      -CertVsys is optional. If omitted, the API places the certificate in shared
    .NOTES
-   Config "show" actions retrieve active configuration.
-   Config "get" actions retrieve candidate, uncommitted configuration.
-
-   Config "show" actions only work when provided XPath specifies single node.
-   Config "get" actions work with single and multiple nodes.
-
-   Config "show" actions can use relative XPath.
-   Config "get" actions require absolute XPath.
-
-   Config "set" is to add, update, or merge configuration nodes. Config "set" actions are non-destructive and are only additive.
-   Config "edit" replaces configuration nodes. Config "edit" actions can be destructive.
    .INPUTS
    PanDevice[]
       You can pipe a PanDevice to this cmdlet
@@ -33,6 +44,12 @@ function Invoke-PanXApi {
    PS> Invoke-PanXApi -Device $Device -Config -Set -XPath "/config/xpath" -Element "<outer><inner>value</inner></outer>"
    .EXAMPLE
    PS> Invoke-PanXApi -Device $Device -Uid -Cmd "<uid-message>...</uid-message>"
+   .EXAMPLE
+   Import and process the certificate and private key within, note the -Category keypair
+   PS> Invoke-PanXApi -Device $Device -Import -Category keypair -File "C:\path\to\cert.p12" -CertificateName "gp-portal-acme-com" -CertPassphrase "acme1234"
+
+   Import and process just the certificate, ignoring the private key, note the -Category certificate. The -CertPassphrase is ignored by API and is not required.
+   PS> Invoke-PanXApi -Device $Device -Import -Category certificate -File "C:\path\to\cert.p12" -CertificateName "gp-portal-acme-com" -CertPassphrase "acme1234"
    #>
    [CmdletBinding()]
    param(
@@ -65,15 +82,15 @@ function Invoke-PanXApi {
       [parameter(Mandatory=$true,Position=1,ParameterSetName='Config-Edit',HelpMessage='Type: config ')]
       [parameter(Mandatory=$true,Position=1,ParameterSetName='Config-Delete',HelpMessage='Type: config ')]
       [Switch] $Config,
-      [parameter(Mandatory=$true,Position=2,ParameterSetName='Config-Get',HelpMessage='Config get mode switch parameter')]
+      [parameter(Mandatory=$true,Position=2,ParameterSetName='Config-Get',HelpMessage='Retrieve candidate configuration')]
       [Switch] $Get,
-      [parameter(Mandatory=$true,Position=2,ParameterSetName='Config-Show',HelpMessage='Config show mode switch parameter')]
+      [parameter(Mandatory=$true,Position=2,ParameterSetName='Config-Show',HelpMessage='Retrieve active configuration')]
       [Switch] $Show,
-      [parameter(Mandatory=$true,Position=2,ParameterSetName='Config-Set',HelpMessage='Config set mode switch parameter')]
+      [parameter(Mandatory=$true,Position=2,ParameterSetName='Config-Set',HelpMessage='Add or create a new object')]
       [Switch] $Set,
-      [parameter(Mandatory=$true,Position=2,ParameterSetName='Config-Edit',HelpMessage='Config edit mode switch parameter')]
+      [parameter(Mandatory=$true,Position=2,ParameterSetName='Config-Edit',HelpMessage='Replace existing object (or hierarchy)')]
       [Switch] $Edit,
-      [parameter(Mandatory=$true,Position=2,ParameterSetName='Config-Delete',HelpMessage='Config delete mode switch parameter')]
+      [parameter(Mandatory=$true,Position=2,ParameterSetName='Config-Delete',HelpMessage='Delete an object')]
       [Switch] $Delete,
       [parameter(Mandatory=$true,ParameterSetName='Config-Get',HelpMessage='Config XPath')]
       [parameter(Mandatory=$true,ParameterSetName='Config-Show',HelpMessage='Config XPath')]
@@ -86,7 +103,46 @@ function Invoke-PanXApi {
       [parameter(Mandatory=$true,ParameterSetName='Config-Set',HelpMessage='Config Element')]
       [parameter(Mandatory=$true,ParameterSetName='Config-Edit',HelpMessage='Config Element')]
       [parameter(ParameterSetName='Config-Delete',HelpMessage='Config Element')]
-      [String] $Element
+      [String] $Element,
+      # Begin Import parameter set
+      [parameter(Mandatory=$true,Position=1,ParameterSetName='Import-Default',HelpMessage='Type: import')]
+      [parameter(Mandatory=$true,Position=1,ParameterSetName='Import-Cert-Key',HelpMessage='Type: import')]
+      [Switch] $Import,
+      [parameter(Mandatory=$true,Position=2,ParameterSetName='Import-Default',HelpMessage='File path to upload')]
+      [parameter(Mandatory=$true,Position=2,ParameterSetName='Import-Cert-Key',HelpMessage='File path to upload')]
+      [System.IO.FileInfo] $File,
+      [parameter(Mandatory=$true,Position=3,ParameterSetName='Import-Default',HelpMessage='Import category: certificate, software, etc.')]
+      [parameter(Mandatory=$true,Position=3,ParameterSetName='Import-Cert-Key',HelpMessage='Import category: certificate, software, etc.')]
+      [ValidateSet(
+         # Software
+         'software',
+         # Content
+         'anti-virus','content','url-database','signed-url-database',
+         # Licenses
+         'license',
+         # Configuration
+         'configuration',
+         # Certificates/Keys
+         'certificate','high-availability-key','keypair',
+         # Response Pages
+         'application-block-page','captive-portal-text','file-block-continue-page','file-block-page','global-protect-portal-custom-help-page',
+         'global-protect-portal-custom-login-page','global-protect-portal-custom-welcome-page','ssl-cert-status-page','ssl-optout-text',
+         'url-block-page','url-coach-text','virus-block-page',
+         # Clients
+         'global-protect-client',
+         # Custom Logo
+         'custom-logo'
+      )]
+      [String] $Category,
+      [parameter(Mandatory=$true,ParameterSetName='Import-Cert-Key',HelpMessage='Certificate friendly name')]
+      [String] $CertName,
+      [parameter(Mandatory=$true,ParameterSetName='Import-Cert-Key',HelpMessage='Certificate format: pkcs12, pem')]
+      [ValidateSet('pkcs12','pem')]
+      [String] $CertFormat,
+      [parameter(ParameterSetName='Import-Cert-Key',HelpMessage='Required when including the certificate private key')]
+      [String] $CertPassphrase,
+      [parameter(ParameterSetName='Import-Cert-Key',HelpMessage='Optional. If empty, defaults to shared')]
+      [String] $CertVsys
    )
 
    Begin {
@@ -99,7 +155,7 @@ function Invoke-PanXApi {
    } # Begin block
 
    Process {
-      foreach($DeviceCur in $Device) {
+      foreach($DeviceCur in $PSBoundParameters.Device) {
          # Set environment's x.509 Certificate Validation Policy
          if ($DeviceCur.ValidateCertificate) {
             Set-X509CertificateValidation -Validate
@@ -149,7 +205,7 @@ function Invoke-PanXApi {
          elseif ($PSCmdlet.ParameterSetName -eq 'Op') {
             Write-Debug ($MyInvocation.MyCommand.Name + ': type=op')
             $PanApiType = 'op'
-            $PanApiCmd = $Cmd
+            $PanApiCmd = $PSBoundParameters.Cmd
             $InvokeParams = @{
                'Method' = 'Post';
                'Uri' = $('{0}://{1}:{2}/api' -f `
@@ -169,7 +225,7 @@ function Invoke-PanXApi {
          elseif ($PSCmdlet.ParameterSetName -eq 'Uid') {
             Write-Debug ($MyInvocation.MyCommand.Name + ': type=user-id')
             $PanApiType = 'user-id'
-            $PanApiCmd = $Cmd
+            $PanApiCmd = $PSBoundParameters.Cmd
             $InvokeParams = @{
                'Method' = 'Post';
                'Uri' = $('{0}://{1}:{2}/api' -f `
@@ -214,25 +270,25 @@ function Invoke-PanXApi {
          elseif ($PSCmdlet.ParameterSetName -like 'Config-*') {
             Write-Debug ($MyInvocation.MyCommand.Name + ': type=config')
             $PanApiType = 'config'
-            if ($Get.IsPresent) {
+            if ($PSBoundParameters.Get.IsPresent) {
                $PanApiAction = 'get'
             }
-            elseif ($Show.IsPresent) {
+            elseif ($PSBoundParameters.Show.IsPresent) {
                $PanApiAction = 'show'
             }
-            elseif ($Set.IsPresent) {
+            elseif ($PSBoundParameters.Set.IsPresent) {
                $PanApiAction = 'set'
             }
-            elseif ($Edit.IsPresent) {
+            elseif ($PSBoundParameters.Edit.IsPresent) {
                $PanApiAction = 'edit'
             }
-            elseif ($Delete.IsPresent) {
+            elseif ($PSBoundParameters.Delete.IsPresent) {
                $PanApiAction = 'delete'
             }
             Write-Debug ($MyInvocation.MyCommand.Name + ": action=$PanApiAction")
 
-            $PanApiXPath = $XPath
-            $PanApiElement = $Element
+            $PanApiXPath = $PSBoundParameters.XPath
+            $PanApiElement = $PSBoundParameters.Element
 
             # Element is optional for several actions. If present, include in the Uri.
             if (-not [String]::IsNullOrEmpty($PanApiElement)) {
@@ -270,6 +326,69 @@ function Invoke-PanXApi {
                } # InvokeParams hash table
             }
          } # End API type=config
+
+         # API type=import
+         elseif ($PSCmdlet.ParameterSetName -like 'Import-*') {
+            Write-Debug ($MyInvocation.MyCommand.Name + ': type=Import')
+            $PanApiType = 'import'
+            Write-Debug ($MyInvocation.MyCommand.Name + ': category=' + $Category)
+
+            if($PSCmdlet.ParameterSetName -eq 'Import-Default') {
+               Write-Debug ($MyInvocation.MyCommand.Name + ": type=Import (Default)")
+               $InvokeParams = @{
+                  'Method' = 'Post';
+                  'Uri' = $('{0}://{1}:{2}/api?key={3}&type={4}&category={5}' -f `
+                     $DeviceCur.Protocol,
+                     $DeviceCur.Name,
+                     $DeviceCur.Port,
+                     $(New-Object -TypeName PSCredential -ArgumentList 'user',$DeviceCur.Key).GetNetworkCredential().Password,
+                     $PanApiType,
+                     $PSBoundParameters.Category
+                  ); # Uri sub-expression
+               } # InvokeParams hash table
+
+               # Submit file to New-MultipartFormData for generating Content-Type header and multipart/form-data encoded body
+               # Make sure to use -UnquotedBoundary for PAN-OS XML API limitation/workaround. See New-MultipartFormData
+               $MPFData = New-MultipartFormData -File $PSBoundParameters.File -UnquotedBoundary
+               # Add ContentType header to InvokeParams
+               $InvokeParams.Add('ContentType', $MPFData.Header.ContentType)
+               $InvokeParams.Add('Body', $MPFData.Body)
+
+            } # end ParameterSetName Import-Default
+
+            elseif($PSCmdlet.ParameterSetName -eq 'Import-Cert-Key') {
+               Write-Debug ($MyInvocation.MyCommand.Name + ": type=Import (Cert-Key)")
+               $InvokeParams = @{
+                  'Method' = 'Post';
+                  'Uri' = $('{0}://{1}:{2}/api?key={3}&type={4}&category={5}&certificate-name={6}&format={7}' -f `
+                     $DeviceCur.Protocol,
+                     $DeviceCur.Name,
+                     $DeviceCur.Port,
+                     $(New-Object -TypeName PSCredential -ArgumentList 'user',$DeviceCur.Key).GetNetworkCredential().Password,
+                     $PanApiType,
+                     $PSBoundParameters.Category,
+                     $PSBoundParameters.CertName,
+                     $PSBoundParameters.CertFormat
+                  ); # Uri sub-expression
+               } # InvokeParams hash table
+
+               # Submit file to New-MultipartFormData for generating Content-Type header and multipart/form-data encoded body
+               # Make sure to use -UnquotedBoundary for PAN-OS XML API limitation/workaround. See New-MultipartFormData
+               $MPFData = New-MultipartFormData -File $File -UnquotedBoundary
+               # Add ContentType header to InvokeParams
+               $InvokeParams.Add('ContentType', $MPFData.Header.ContentType)
+               $InvokeParams.Add('Body', $MPFData.Body)
+
+               # If Certificate passphrase is specified, include it in Uri
+               if($PSBoundParameters.ContainsKey('CertPassphrase')){
+                  $InvokeParams.Uri += '&passphrase={0}' -f $CertPassphrase
+               }
+               # If Certificate vsys is specified, include it in Uri. If omitted, API defaults to shared
+               if($PSBoundParameters.ContainsKey('CertVsys')){
+                  $InvokeParams.Uri += '&vsys={0}' -f $CertVsys
+               }
+            } # end ParameterSetName Import-Cert-Key
+         } # End API type=import
 
          # Call PAN-OS XML-API with PowerShell built-in Invoke-WebRequest, include some debug
          # Invoke-WebRequest is preferred over Invoke-RestMethod. In PowerShell 5.1, Invoke-RestMethod does not make HTTP response
